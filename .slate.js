f73b43c35070679f3c0413dcc0f1eb6d28c4217a
therealplato <therealplato@gmail.com>
2016-03-28 09:09:31 -0400

    Rename some vars, prepare for layout

--- .slate.js
+++ .slate.js
@@ -1,33 +1,43 @@
+(function(){
 var FULL = {"x": "screenOriginX", "y": "screenOriginY", "width": "screenSizeX", "height": "screenSizeY"};
 var fullscreen = slate.op("move", FULL);
 var HALF = "bar-resize:screenSizeX/2";
-var toL = slate.op("push", {"direction": "left", "style": HALF });
-var toR = slate.op("push", {"direction": "right","style": HALF });
-var fL = slate.op("focus", {"direction": "left" });
-var fR = slate.op("focus", {"direction": "right" });
+var opPushL = slate.op("push", {"direction": "left", "style": HALF });
+var opPushR = slate.op("push", {"direction": "right","style": HALF });
+var opFocusLeft = slate.op("focus", {"direction": "left" });
+var opFocusRight = slate.op("focus", {"direction": "right" });
+
+var toNewScreenFullscreen = function(win){
+  var WINS = [];
+}
 
 //slate.source("~/.slatetile.js")
+//place and focus new window at index 0:
+//place and focus new window to the left of focused
+//
+//place and focus new window to the right of focused
 
 slate.bindAll({
   ";:alt": fullscreen,
   ".:alt": toggleMinimized,
   "/:alt": slate.op("show", {"app": "Terminal"}),
-  "h:alt": toL, // win to left half
-  "l:alt": toR, // win to right half
-  "j:alt": fL,  // focus leftwards
-  "k:alt": fR,  // focus rightwards
+  "h:alt": opPushL, // win to left half
+  "l:alt": opPushR, // win to right half
+  "j:alt": opFocusLeft,  // focus leftwards
+  "k:alt": opFocusRight,  // focus rightwards
   //"q:alt,shift": slate.op("relaunch"), // doesn't appear to resource .slate.js?
   //"/:alt:shift": spawnTerm,
 });
 
+var lastToggledOut = "";
 function toggleMinimized(win){
-  var last = "";
   var current = win.app().name();
-  if( last != "") { // we remembered something to switch 
-    win.doOperation(slate.op("show", {"app": last}));
-    last = ""
+  if( lastToggledOut != "") { // we remembered something to switch 
+    win.doOperation(slate.op("show", {"app": lastToggledOut}));
+    win.doOperation(slate.op("focus", {"app": lastToggledOut}));
+    lastToggledOut = "";
   } else {
-    last = current;
+    lastToggledOut = current;
     win.doOperation(slate.op("hide", {"app": "current" }));
   }
 };
@@ -35,4 +45,4 @@ function toggleMinimized(win){
 function spawnTerm(win) {
   slate.shell("open -a Terminal.app . &> ~/.slate.err.log") // wrong, help?
 };
-
+})();
