45f46d3e0cc74aa826f769697568ed04166c23ec
Isaac Rogers <therealplato@gmail.com>
2017-10-01 14:10:55 +1300

    whatever, colors and stuff

---
+++ themes/subversive-0.2.0.js
@@ -0,0 +1,42 @@
+exports.colors = {
+  dark: { // A color set can have both light and dark variants, but doesn't have to.
+    accent0: '#0b0b0b', // accent0-7 should be the main accent colors of your theme.
+    accent1: '#d26b6e',
+    accent2: '#69ef64',
+    accent3: '#e3d486',
+    accent4: '#79b6c7',
+    accent5: '#da5581',
+    accent6: '#1c768b',
+    accent7: '#404040',
+
+    shade0: '#171d22', // shade0-7 should be shades of the same hue, with shade0 being the darkest and shade7 being the lightest.
+    shade1: '#3f3f3f', // shade0-7 should be shades of the same hue, with shade0 being the darkest and shade7 being the lightest.
+    shade2: '#3f3f3f', // shade0-7 should be shades of the same hue, with shade0 being the darkest and shade7 being the lightest.
+    shade3: '#3f3f3f', // shade0-7 should be shades of the same hue, with shade0 being the darkest and shade7 being the lightest.
+    shade4: '#efefef',
+    shade5: '#efefef',
+    shade6: '#efefef',
+    shade7: '#c0f5c5',
+  },
+//  light: { ... }, // same as above, except that shade0 should be the lightest and shade7 should be the darkest.
+};
+// {
+//   "0": "#0b0b0b",
+//   "1": "#d26b6e",
+//   "2": "#69ef64",
+//   "3": "#e3d486",
+//   "4": "#79b6c7",
+//   "5": "#da5581",
+//   "6": "#1c768b",
+//   "7": "#404040",
+//   "8": "#3f3f3f",
+//   "9": "#f2b06e",
+//   "10": "#7085fa",
+//   "11": "#63e5a2",
+//   "12": "#6eb4f1",
+//   "13": "#fc26a3",
+//   "14": "#72d9f4",
+//   "15": "#efefef",
+//   "background": "#171d22",
+//   "foreground": "#c0f5c5"
+// }
