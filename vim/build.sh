954f5f268e21aba844d27e91980eb02ae3236ba8
Isaac Rogers <therealplato@gmail.com>
2018-02-07 16:13:51 +1300

    prepare vimfiles-osx

--- vim/build.sh
+++ vim/build.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
-<<<<<<< HEAD
-#cp -v $HOME/.vimrc $HOME/.slate.js $HOME/.hyper.js $HOME/.zshrc $HOME/.screenrc .
-#cp -v $HOME/.vim/*.vimrc ./.vim/
-#cp -v $HOME/zsh/gitps.sh $HOME/zsh/plato.sh zsh
-=======
 set -e
 FORCE=0
-V=./vimfiles
-VRC=./_vimrc
+V=./vimfiles-osx
+VRC=./.vimrc
+if [ -z ${OS+x} ]; then
+    echo -e "Env OS not set, assuming osx"
+    OS="osx"
+fi
 
 while test $# -gt 0; do
   case "$1" in
     -f|--f|--force)
 	  FORCE=1
 	  break
-      ;;		
+      ;;
     *)
       break
       ;;
@@ -34,9 +33,12 @@ cat $V/binds.vimrc >> $VRC
 echo "\" visual.vimrc:" >> $VRC
 cat $V/visual.vimrc >> $VRC
 
-echo "generated $(wc -l $VRC | grep -Eo ^[0-9]+) lines of _vimrc"
+echo "generated $(wc -l $VRC | grep -Eo ^[0-9]+) lines of vimrc"
 
 if [ $FORCE -eq 1 ]; then
-	cp -vi $VRC $HOME/_vimrc
+  if [ $OS -eq "windows" ]; then
+    cp -vi $VRC $HOME/_vimrc
+  else
+    cp -vi $VRC $HOME/.vimrc
+  fi
 fi
->>>>>>> windows
