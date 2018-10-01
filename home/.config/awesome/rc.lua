960086713cdcb312f857b3f7fc997609bf366003
Isaac <therealplato@gmail.com>
2018-10-01 19:36:26 +1300

    awesome: duplicate default theme

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -48,7 +48,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(gears.filesystem.get_xdg_config_home()  .. "/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "cool-retro-term"
